62bef6f6b027d89e575f948d9f799128d0d6deb9
wish9 <wjwee9@gmail.com>
2023-03-14 12:22:52 +0900

    HTTPS 연습 - mkcert이용 로컬 환경 테스트

---
+++ src/main/java/com/wish/practicehttps/PracticeHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.wish.practicehttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeHttpsApplication.class, args);
+	}
+
+}
